9be40524e9f95e88c59d531609abd1f6c9d126e9
Simeon Yordanov <lookmanche@gmail.com>
2022-11-29 11:26:44 +0200

    added in-memory class to persist the cities

---
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,34 @@
+﻿namespace CityInfo.API
+{
+    using CityInfo.API.Models;
+
+    public class CitiesDataStore
+    {
+        public CitiesDataStore()
+        {
+            this.Cities = new List<CityDto>
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park",
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really fired",
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Paris",
+                    Description = "The one with the big tower",
+                },
+            };
+        }
+
+        public List<CityDto> Cities { get; set; }
+    }
+}
--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -6,8 +6,8 @@
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-        //[HttpGet("api/cities")] this is used if we dont specify [Route("api/cities")] attribute on the top of the controller
-        [HttpGet()]
+        //[HttpGet("api/cities")] => this is used if we dont specify [Route("api/cities")] attribute on the top of the controller
+        [HttpGet]
         public JsonResult GetCities()
         {
             return new JsonResult(new List<object>
@@ -18,3 +18,4 @@
         }
     }
 }
+--====
