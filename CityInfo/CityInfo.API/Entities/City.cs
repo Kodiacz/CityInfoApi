0e0ecd2db9e94bd819246db9d76c9a2d6045733a
Simeon Yordanov <lookmanche@gmail.com>
2022-11-30 11:17:43 +0200

    creating entities folder
    created City and PointOfInterest entities

--- CityInfo/CityInfo.API/Entities/City.cs
+++ CityInfo/CityInfo.API/Entities/City.cs
@@ -1,6 +1,23 @@
-﻿namespace CityInfo.API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
 {
     public class City
     {
+        public City()
+        {
+            this.PointOfInteresets = new List<PointOfIntereset>();  
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public ICollection<PointOfIntereset> PointOfInteresets { get; set; }
     }
 }
---
+++ CityInfo/CityInfo.API/Entities/PointOfIntereset.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfIntereset
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        [ForeignKey(nameof(City))]
+        public int CityId { get; set; }
+        public City? City { get; set; }
+    }
+}
