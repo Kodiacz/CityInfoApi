60456477c3e110403682c537657500dd5af406e5
Simeon Yordanov <lookmanche@gmail.com>
2022-12-24 18:09:30 +0200

    deleted the default implementation in index.js

---
+++ Reac-18-Fundamentals/globamantics/components/banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+    return (
+        <header>
+            <div>
+                <img src="./GloboLogo.png" alt="logo"></img>
+            </div>
+            <div>
+                Providing houses all over the world
+            </div>
+        </header>
+    )
+}
+
+export default Banner;
